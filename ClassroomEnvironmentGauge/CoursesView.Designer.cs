d17fefac7913e37229ece2151d14c54cbeec4bc4
Mr Helland <tim.helland@lebanon.k12.or.us>
2021-11-11 10:24:22 -0800

    Add reusable CourseView.

---
+++ ClassroomEnvironmentGauge/CoursesView.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace ClassroomEnvironmentGauge {
+    partial class CoursesView {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dgvCourses = new System.Windows.Forms.DataGridView();
+            this.Period = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CourseName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bsCourses = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsCourses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCourses
+            // 
+            this.dgvCourses.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.dgvCourses.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCourses.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Period,
+            this.CourseName});
+            this.dgvCourses.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCourses.Location = new System.Drawing.Point(0, 0);
+            this.dgvCourses.Name = "dgvCourses";
+            this.dgvCourses.Size = new System.Drawing.Size(493, 405);
+            this.dgvCourses.TabIndex = 1;
+            // 
+            // Period
+            // 
+            this.Period.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Period.DataPropertyName = "Period";
+            this.Period.HeaderText = "Period";
+            this.Period.MaxInputLength = 4;
+            this.Period.MinimumWidth = 30;
+            this.Period.Name = "Period";
+            this.Period.Width = 50;
+            // 
+            // CourseName
+            // 
+            this.CourseName.DataPropertyName = "Name";
+            this.CourseName.HeaderText = "Course Name";
+            this.CourseName.MaxInputLength = 100;
+            this.CourseName.MinimumWidth = 100;
+            this.CourseName.Name = "CourseName";
+            this.CourseName.Width = 300;
+            // 
+            // CoursesView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.dgvCourses);
+            this.Name = "CoursesView";
+            this.Size = new System.Drawing.Size(493, 405);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsCourses)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCourses;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Period;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CourseName;
+        private System.Windows.Forms.BindingSource bsCourses;
+    }
+}
---
+++ ClassroomEnvironmentGauge/CoursesView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassroomEnvironmentGauge {
+    public partial class CoursesView : UserControl {
+
+        private List<CourseSection> courses;
+        public List<CourseSection> Courses {
+            get => courses;
+            set {
+                courses = value;
+                this.bsCourses.DataSource = courses;
+                this.dgvCourses.DataSource = this.bsCourses;
+            }
+        }
+
+        private bool isReadOnly = false;
+        public bool ReadOnly {
+            get => isReadOnly;
+            set { 
+                isReadOnly = value;
+                dgvCourses.ReadOnly = isReadOnly;
+            }
+        }
+
+        public CoursesView() {
+            InitializeComponent();
+        }
+
+        public void SaveChanges() {
+            courses.Sort((CourseSection A, CourseSection B) => A.Period.CompareTo(B.Period));
+            CourseSection.SetCourses(courses);
+        }
+
+
+    }
+}
